64945da0421d1789c0afea23808357c99c4d57df
B <bkashishh07@gmail.com>
2024-10-07 16:18:45 +0530

    changes

--- client/app/lib/auth.ts
+++ client/app/lib/auth.ts
@@ -26,7 +26,7 @@ export const NEXT_AUTH = {
 
         try {
           const response = await axios.post(
-            "http://3.110.101.31:3000/user/userregistration",
+            `${process.env.AWS_HOSTED_URL}/user/userregistration`,
 
             {
               name,
